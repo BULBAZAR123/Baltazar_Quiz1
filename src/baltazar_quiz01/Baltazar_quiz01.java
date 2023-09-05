47e9d204e815001de4f9d62db6c3dc3459f74000
Student <Student@CLB3Unit33>
2023-09-05 17:31:58 +0800

    Version1

--- src/baltazar_quiz01/Baltazar_quiz01.java
+++ src/baltazar_quiz01/Baltazar_quiz01.java
@@ -5,6 +5,9 @@
  */
 package baltazar_quiz01;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Student
@@ -16,6 +19,50 @@ public class Baltazar_quiz01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Code1();
     }
+
+public static void Code1(){
+        int[] OrigArr = {82, 44, 96, 7, 13, 56, 74, 14, 98, 36};
+        System.out.println("Your Array: " + Arrays.toString(OrigArr));
+        
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter an Array: ");
+        int num = scn.nextInt();
+        
+        System.out.println("Choose a the Array you want to be Inserted: ");
+        System.out.println(Arrays.toString(OrigArr));
+        int element = scn.nextInt();
+        
+  if(element > 0 && element <= OrigArr.length){   
+      
+      int[] newArray = new int[OrigArr.length + 1];
+  
+        for (int i = 0; i <= element; i++) {
+            newArray[i] = OrigArr[i];
+    }
+        
+        newArray[element] = num;
+       
+        for (int i = element; i < OrigArr.length; i++) {
+        newArray[i + 1] = OrigArr[i];
+    }
+        
+        System.out.println("Orig Array: " + Arrays.toString(OrigArr));
+        System.out.println("Element Inserted" + num);
+        System.out.println("New Array" + Arrays.toString(newArray));
+    }
+  
+}
+
+public static void Code3(){
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter an how many arrays: ");
+    int arrays = scn.nextInt();
+    System.out.println("Enter Elements: ");
+    int elements = scn.nextInt();
+    
+    int[] origarr;
     
 }
+    }
